6725e98ebd98fe7dd6bc1c749bb14a84f5e2b794
Armand Mosnier <armosnie@f4r10s14.clusters.42paris.fr>
2025-12-11 16:18:45 +0100

    wtf

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -6,8 +6,43 @@
 /*   By: armosnie <armosnie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 16:41:07 by armosnie          #+#    #+#             */
-/*   Updated: 2025/12/10 16:41:17 by armosnie         ###   ########.fr       */
+/*   Updated: 2025/12/11 16:11:32 by armosnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
+#include "Fixed.hpp"
+
+Point::Point() : _x(0), _y(0) {}
+
+Point::~Point() {}
+
+Point::Point(float const x, float const y) : _x(x), _y(y) {}
+
+Point::Point(Point const &pointCopy) : _x(pointCopy._x), _y(pointCopy._y) {}
+
+Point &Point::operator=(Point const &pointCopy) {
+    
+    return *this;
+}
+
+Fixed Point::getX() const {
+    
+    return _x;
+}
+
+Fixed Point::getY() const {
+    
+    return _y;
+}
+
+float calculate_area(Point const a, Point const b, Point const c) {
+
+    Fixed result = a.getX() * (b.getY() - c.getY()) + b.getX() * (a.getY() - c.getY()) + c.getX() * (a.getY() - b.getY()) +
+}
+
+bool bsp(Point const a, Point const b, Point const c, Point const point) {
+    
+    float triangle = calculate_area(a, b, c);
+
+}
--- ex03/Point.hpp
+++ ex03/Point.hpp
@@ -1,14 +1,26 @@
 #ifndef POINT_HPP
 #define POINT_HPP
 
-class Point {
+#include "Fixed.hpp"
+
+class Point {   
 
     private :
-
+        Fixed const _x;
+        Fixed const _y;
 
     public :
 
+        Point();
+        ~Point();
+        Point(float const x, float const y);
+        Point(Point const &pointCopy);
+        Point &operator=(Point const &pointCopy);
+        Fixed getX() const;
+        Fixed getY() const;
+    };
 
-};
+    float calculate_area(Point const a, Point const b, Point const c);
+    bool bsp( Point const a, Point const b, Point const c, Point const point);
 
 #endif
